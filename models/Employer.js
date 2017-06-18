db6d527307b1601b04d665eafc6fd518d349e05c
Rukaroa <rukaroa@gmail.com>
2017-06-18 17:03:19 -0500

    base models

---
+++ models/Employer.js
@@ -0,0 +1,9 @@
+/*
+Guideline
+-Name
+-Company Description
+-URL
+-Available jobs
+-reviews, only talent that have done work for employers can review or rate upon success or fail
+-ratings
+*/
---
+++ models/Talent.js
@@ -0,0 +1,12 @@
+/*
+Guideline
+-Name
+-Job Title
+-Services
+-Skills
+-Certifications
+-Previous jobs
+-Hourly price
+-reviews, only employers that have done jobs with talent can review and rate upon success or fail
+-ratings
+*/

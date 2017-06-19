d1eb29fdb44781fd5ad86daab0f48d04e9b2bb1f
Jesse Thomas <jayayetea@gmail.com>
2017-06-19 17:05:51 -0500

    added both the employer and talent models, also changed app.js target inside of webpack.config.js

--- models/Employer.js
+++ models/Employer.js
@@ -7,3 +7,59 @@ Guideline
 -reviews, only talent that have done work for employers can review or rate upon success or fail
 -ratings
 */
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var EmployerSchema = new Schema({
+	id: {
+    	type: String
+    },
+    name: {
+    	type: String
+    },
+    companyname: {
+    	type: String
+    },
+    url: {
+    	type: String
+    },
+    jobs: [{ 
+  		title: {
+  			type: String
+  		},
+  		description: {
+  			type: String
+  		},
+  		date: {
+  			type: Date
+  		},
+  		taken: {
+  			type: Boolean
+  		}
+    }],
+    meta: {
+    	reviews: [{
+    		id: {
+    			type: String
+    		},
+    		reviewer: {
+    			type: String
+    		},
+    		body: {
+    			type: String
+    		},
+    		date: {
+    			type: Date
+    		}
+    	}],
+    	ratings: [{
+    		value: {
+    			type: Number
+    		}
+    	}]
+    }
+});
+
+var Employer = mongoose.model("Employer", EmployerSchema);
+module.exports = Employer;
--- models/Talent.js
+++ models/Talent.js
@@ -10,3 +10,83 @@ Guideline
 -reviews, only employers that have done jobs with talent can review and rate upon success or fail
 -ratings
 */
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var TalentSchema = new Schema({
+	id: {
+    	type: String
+    },
+    name: {
+    	type: String
+    },
+    jobtitle: {
+    	type: String
+    },
+    services: {
+    	type: String
+    },
+    skills : [{
+    	description: {
+    		type: String
+    	}
+    }],
+    certs: {
+    	title: {
+    		type: String
+    	},
+    	img: {
+    		type: String
+    	}
+    },
+    previousjobs: [{
+    	title: {
+    		type: String
+    	},
+    	responsibilities: [{
+    		body: {
+    			type: String,
+    		}
+    	}],
+    	left: {
+    		type: Date
+    	},
+    	references: [{
+    		name: {
+    			type: String
+    		},
+    		contact: {
+    			type: String
+    		}
+    	}]
+    }],
+    hourly: {
+    	type: Number
+    },
+    meta: {
+    	reviews: [{
+    		id: {
+    			type: String
+    		},
+    		reviewer: {
+    			type: String
+    		},
+    		body: {
+    			type: String
+    		},
+    		date: {
+    			type: Date
+    		}
+    	}],
+    	ratings: [{
+    		value: {
+    			type: Number
+    		}
+    	}]
+    }
+    
+});
+
+var Talent = mongoose.model("Talent", TalentSchema);
+module.exports = Talent;

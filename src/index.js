bb9be186031ba0ba3f4a801ab669af4cfdd8e23e
Rukaroa <rukaroa@gmail.com>
2017-06-20 15:28:29 -0500

    semantic added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import routes from "../app/config/routes";
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();

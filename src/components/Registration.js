7cf54b0d93475baacd35fccbb6a7a478c3a47d2b
rukaroa <rukaroa@gmail.com>
2017-06-20 21:03:47 -0500

    basic registration made

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,30 +3,56 @@ import React, {Component} from "react";
 class Registration extends Component {
     render() {
         return (
-            <div className="ui container">
-                <div className="ui block header">Registration</div>
-                {/*name, individual/organization, email/contact, password*/}
-                <div className="row labeled input">
-                    <div className="ui label">Name</div>
-                    <input type="text"/>
+            <div>
+                <div className="col-md-4"></div>
+                <div className="col-md-4">
+                    <div className="panel panel-default">
+                        <div className="panel-heading">Registration</div>
+                        <div className="panel-body">
+                            <form>
+                                <div className="form-group">
+                                {/*name*/}
+                                    <div className="input-group">
+                                        <div className="input-group-addon">Name</div>
+                                        <input type="text" className="form-control" id="reg-name"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                {/*email*/}
+                                    <div className="input-group">
+                                        <div className="input-group-addon">Email</div>
+                                        <input type="text" className="form-control" id="reg-email"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                {/*account type*/}
+                                    <div className="input-group">
+                                        <div className="input-group-addon">Account Type</div>
+                                        <select className="form-control">
+                                            <option>Individual</option>
+                                            <option>Employer</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    {/*password*/}
+                                    <div className="input-group">
+                                        <div className="input-group-addon">Password</div>
+                                        <input type="text" className="form-control" id="password"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    {/*password confirm*/}
+                                    <div className="input-group">
+                                        <div className="input-group-addon">Confirm Password</div>
+                                        <input type="text" className="form-control" id="confirm-pw"/>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div className="row dropdown">
-                    <option value="">Account Type</option>
-                    <option value="talent">Individual</option>
-                    <option value="employer">Employer</option>
-                </div>                
-                <div className="row labeled input">
-                    <div className="ui label">Email</div>
-                    <input type="text"/>
-                </div>
-                <div className="row labeled input">
-                    <div className="ui label">Password</div>
-                    <input type="text"/>
-                </div>
-                <div className="row labeled input">
-                    <div className="ui label">Confirm Password</div>
-                    <input type="text"/>
-                </div>
+                <div className="col-md-4"></div>
             </div>
         );
     }
